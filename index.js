8f7cc825016851f2768c871d5d0843eed28e10f5
arta fallahpoor <102357381+artafallahpoor@users.noreply.github.com>
2022-06-30 17:42:18 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var vo = document.getElementsByClassName(
+  "_13NKt copyable-text selectable-text"
+)[1];
+var ho = new InputEvent("input", { bubbles: true });
+vo.innerHTML = `text`;
+vo.dispatchEvent(ho);
+document
+  .getElementsByClassName("tvf2evcx")
+  [document.getElementsByClassName("tvf2evcx").length - 1].click();
+
